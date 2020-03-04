151e77ed1053a75504cfb9528581582968dcc716
Nick Hu <nick@nickhu.info>
2020-03-04 23:03:07 +0800

    fix commands

--- src/components/Input/Commands.ts
+++ src/components/Input/Commands.ts
@@ -6,8 +6,8 @@ const content = {
     bio: Learn more about me\n
     contact: See how to contact the me\n
     examples: Discover what else I have made\n
-    background: change the background colour e.g., background orange\n
-    text: change the text colour e.g., text black,
+    background: change the background colour e.g., type background orange\n
+    text: change the text colour e.g., type text black\n
     reset: start all over
     `,
   version: '1.0.0',
@@ -16,7 +16,7 @@ const content = {
     React + Next.js + Craft CMS + GraphQL is my main JAM. ;)\n
     Javascript is where I feel at home, but i'm not afraid to use PHP, Ruby, Python when need be.`,
   contact: `feel free to email me via <a href="mailto:nick@nickhu.info">nick@nickhu.info</a>,
-    check out my <a href="https://github.com/nicholashu" target="_blank">GitHub</a>, or follow me on <a href="https://www.linkedin.com/in/nick-hu-104b231a1/" target="_blank">linkedIn</a>`,
+    check out my <a href="https://github.com/nicholashu" target="_blank">GitHub</a>, or connect with me on <a href="https://www.linkedin.com/in/nick-hu-104b231a1/" target="_blank">linkedIn</a>`,
   examples: `I currently work for <a href="https://league.agency" target="_blank">The League Agency</a> in Perth, Western Australia as a Full Stack Web Developer.\n
     I‘ve had my hands all over the place. Some of our coolest projects include:\n
     <a href="https://www.discoverychannel.com.au/" target="_blank">Discovery Channel Australia</a>\n
