69a29e3654ffa519215bbef17a4ff60d11cf2a99
Nick Hu <nick.hu@airteam.com.au>
2023-07-31 08:26:54 +0800

    update copy

--- src/components/Input/Commands.ts
+++ src/components/Input/Commands.ts
@@ -10,21 +10,21 @@ const content = {
     text: change the text colour e.g., type text black\n
     reset: start all over
     `,
-  version: '1.0.4',
+  version: '1.0.5',
   bio: `Hi! :D i‘m Nick Hu. I am a web developer based in the picturesque Perth, Western Australia.\n
     I like to create beautiful web applications both inside and out.\n
-    React + Next.js + Craft CMS + GraphQL is my main JAM. ;)\n
-    Javascript is where I feel at home, but i'm not afraid to use PHP, Ruby, Python when need be.`,
+    React + Next.js + Sanity CMS + GraphQL is my main JAM. ;)\n
+    Javascript/Typescript is where I feel at home, but i'm not afraid to use PHP, Ruby, Python when need be.`,
   contact: `feel free to email me via <a href="mailto:nick@nickhu.info">nick@nickhu.info</a>,
-    check out my <a href="https://github.com/nicholashu" target="_blank">GitHub</a>, or connect with me on <a href="https://www.linkedin.com/in/nick-hu-104b231a1/" target="_blank">linkedIn</a>`,
+    check out my <a href="https://github.com/nicholashu" target="_blank">GitHub</a>, or connect with me on <a href="https://www.linkedin.com/in/nick-hu-perth/" target="_blank">linkedIn</a>`,
   examples: `I currently work for <a href="https://airteam.com.au" target="_blank">Airteam</a> remotely from Perth, Western Australia as a Full Stack Web Developer.\n
     I‘ve had my hands all over the place. Some of our coolest projects include:\n
     <a href="#">Discovery Channel Australia (sadly offline :[)</a>\n
-    <a href="https://carz4u.com.au" target="_blank">Carz4U</a>\n
+    <a href="https://mhnsw.au" target="_blank">Museums of History NSW</a>\n
+    <a href="https://www.hbfdental.com.au/" target="_blank">HBF Dental</a>\n
     <a href="https://www.myrewardclub.com.au/" target="_blank">My Reward Club</a>\n
     <a href="https://johnhughes.com.au" target="_blank">John Hughes</a>\n
-    <a href="https://dvg.com.au" target="_blank">DVG</a>\n
-    <a href="https://www.jesters.com.au/" target="_blank">Jesters Pies</a>\n`,
+    <a href="https://dvg.com.au" target="_blank">DVG</a>\n`,
 }
 const available = Object.keys(content);
 
